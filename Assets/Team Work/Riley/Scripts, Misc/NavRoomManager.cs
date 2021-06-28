f7a3902ab69488f2b47b5e2bdf53b70723d148bf
Riley <1017093@student.sae.edu.au>
2021-06-28 12:22:57 +1000

    Ew project files for hdrp

---
+++ Assets/Team Work/Riley/Scripts, Misc/NavRoomManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RileyMcGowan;
+using UnityEngine;
+
+public class NavRoomManager : MonoBehaviour
+{
+    //Private Vars
+    private EnemyNav[] enemyNav;
+    
+    //Public Vars
+    public GameObject[] enemies;
+    public GameObject[] roomNavPoints;
+    
+    void Start()
+    {
+        //On start get all enemynav components 
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemyNav[i] = enemies[i].GetComponent<EnemyNav>();
+        }
+    }
+
+    public void GetNavPoint()
+    {
+        GameObject chosenNavPoint = roomNavPoints[Random.Range(0, roomNavPoints.Length)];
+    }
+    
+}
