5533ea89f0b1702d7a1812a91285346435258892
Riley McGowan <1017093@student.sae.edu.au>
2021-07-05 13:31:36 +1000

    Quick Additions

---
+++ Assets/Team Work/Riley/Scripts, Misc/FOV.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RileyMcGowan
+{
+    public class FOV : MonoBehaviour
+    {
+        //Private Vars
+        
+        //Public Vars
+        
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
