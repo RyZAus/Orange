799f0fb3687bbb5a1627363a26d22e5f1805992c
Riley McGowan <1017093@student.sae.edu.au>
2021-07-19 13:10:11 +1000

    Main Camera Memory Fix

--- Assets/Team Work/Damien/Scripts/MainCamera.cs
+++ Assets/Team Work/Damien/Scripts/MainCamera.cs
@@ -3,28 +3,37 @@
 public class MainCamera : MonoBehaviour
 {
     public Portal[] portals;
-
+    private Portal currentPortal;
+    private int portalLength;
+    
     void Awake()
     {
         portals = FindObjectsOfType<Portal>();
+        portalLength = portals.Length;
     }
 
     void LateUpdate()
     {
         //Debug.Log("PreCull");
-        for (int i = 0; i < portals.Length; i++)
+        for (int i = 0; i < portalLength; i++)
         {
-            portals[i].PrePortalRender();
+            currentPortal = portals[i];
+            PrePortalRenderFunc(currentPortal);
+            PortalRenderFunc(currentPortal);
+            PostPortalRenderFunc(currentPortal);
         }
+    }
 
-        for (int i = 0; i < portals.Length; i++)
-        {
-            portals[i].Render();
-        }
-
-        for (int i = 0; i < portals.Length; i++)
-        {
-            portals[i].PostPortalRender();
-        }
+    void PrePortalRenderFunc(Portal portal)
+    {
+        portal.PrePortalRender();
+    }
+    void PortalRenderFunc(Portal portal)
+    {
+        portal.Render();
+    }
+    void PostPortalRenderFunc(Portal portal)
+    {
+        portal.PostPortalRender();
     }
 }
